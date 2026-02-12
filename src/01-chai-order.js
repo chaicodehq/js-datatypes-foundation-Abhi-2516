ab3f1a6d7002ad50dd118e88446694d5970e5bdb
Abhi-2516 <2516abhi43@gmail.com>
2026-02-12 19:42:36 +0530

    01-chai order ho gya

--- src/01-chai-order.js
+++ src/01-chai-order.js
@@ -46,21 +46,47 @@
  *   hasSpecialIngredient("Elaichi Chai", "elaichi")  // => true
  */
 export function getChaiOrderLength(order) {
-  // Your code here
+  if (typeof order !== "string") return -1;
+
+  const t = order.trim();
+  return t.length;
 }
 
 export function shoutChaiOrder(order) {
-  // Your code here
+  if (typeof order !== "string") return "";
+
+  const ans = order.trim();
+  if (ans === "") return "";
+
+  return ans.toUpperCase();
 }
 
 export function whisperChaiOrder(order) {
-  // Your code here
+  if (typeof order !== "string") return "";
+
+  const t = order.trim();
+  if (t === "") return "";
+
+  return t.toLowerCase();
 }
 
 export function hasSpecialIngredient(order, ingredient) {
-  // Your code here
+  if (typeof order !== "string" || typeof ingredient !== "string") {
+    return false;
+  }
+
+  return order.toLowerCase().includes(ingredient.toLowerCase());
 }
 
 export function getFirstAndLastChar(order) {
-  // Your code here
+  if (typeof order !== "string") return null;
+
+  const t = order.trim();
+  if (t === "") return null;
+
+  return {
+    first: t.charAt(0),
+    last: t.at(-1)
+  };
 }
+
